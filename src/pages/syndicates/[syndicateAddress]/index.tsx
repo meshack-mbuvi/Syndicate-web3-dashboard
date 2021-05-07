5fd2320f184c707eddaec42ea66c9c82376f571e
Jillo Abdullahi <jillo.abdulla@gmail.com>
2021-05-07 11:07:21 +0300

    fix route issue when there is no actions specified (#63)
     - fix an issue with inconsistent states shown when the syndicate details page is opened without an action
     - actions in this case would be manage, deposit, withdraw, or details.
    
    Fixes [SYN-368]

--- src/pages/syndicates/[syndicateAddress]/index.tsx
+++ src/pages/syndicates/[syndicateAddress]/index.tsx
@@ -1,8 +1,16 @@
 // This page deals with deposits or withdrawals to a syndicate.
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import SyndicateActions from "@/containers/syndicateActions";
 
 const SyndicateDepositOrWithdrawPage = () => {
+  const router = useRouter();
+  const { syndicateAddress } = router.query;
+
+  useEffect(() => {
+    // re-route to the deposit page
+    router.replace(`/syndicates/${syndicateAddress}/deposit`);
+  });
   return <SyndicateActions />;
 };
 
