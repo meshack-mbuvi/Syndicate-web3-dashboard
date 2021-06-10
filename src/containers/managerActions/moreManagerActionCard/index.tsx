ce187d364debafe9a36a83f450d22bf7fd57dd0a
Meshack Mbuvi <meshmbuvi@gmail.com>
2021-06-10 18:30:11 +0300

    Add hover state to moreManagerActionCard. (#153)

--- src/containers/managerActions/moreManagerActionCard/index.tsx
+++ src/containers/managerActions/moreManagerActionCard/index.tsx
@@ -8,14 +8,11 @@ interface Props {
 
 const MoreManagerActions = ({ icon, text, onClickHandler }: Props) => {
   return (
-    <div 
-      className="flex bg-gray-9 h-14 items-center rounded-custom px-6 my-4 cursor-pointer"
-      onClick={onClickHandler}
-    >
+    <div
+      className="flex bg-gray-9 bg-opacity-75 h-14 items-center rounded-custom px-6 my-4 cursor-pointer hover:bg-opacity-100"
+      onClick={onClickHandler}>
       <div className="mr-4">{icon}</div>
-      <div className="text-md font-semibold">
-        {text}
-      </div>
+      <div className="text-md font-semibold">{text}</div>
     </div>
   );
 };
