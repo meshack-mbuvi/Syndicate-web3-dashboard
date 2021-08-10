1bc34251170fda4a509842a92af27c5b561d02b0
Jillo Abdullahi <jillo.abdulla@gmail.com>
2021-08-10 14:12:49 +0300

    [ENG-1181][ENG-1184] Fix allow accredited investors text and toggle plus typos (#341)
    * increase font size and add margin right
    
    * fix typo in 'transferrable'

--- src/components/inputs/switchButton/index.tsx
+++ src/components/inputs/switchButton/index.tsx
@@ -11,7 +11,7 @@ const SwitchButton: React.FC<ISwitchButton> = ({ label, enabled, setEnabled }) =
   return (
     <Switch.Group as="div" className="flex items-center justify-between">
       <span className="flex-grow flex flex-col">
-        <Switch.Label as="span" className="text-sm" passive>
+        <Switch.Label as="span" className="text-base" passive>
           {label}
         </Switch.Label>
       </span>
@@ -20,7 +20,7 @@ const SwitchButton: React.FC<ISwitchButton> = ({ label, enabled, setEnabled }) =
         onChange={setEnabled}
         className={classNames(
           enabled ? 'bg-blue-navy' : 'bg-gray-200',
-          'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+          'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 mr-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
         )}
       >
         <span
--- src/containers/create/onChainDetails/componentInfo/transferable/index.tsx
+++ src/containers/create/onChainDetails/componentInfo/transferable/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export const TransferableInfo: React.FC = () => (
   <RightPlaceHolder
-    title="What does it mean for a syndicate to be transferrable?"
+    title="What does it mean for a syndicate to be transferable?"
     body={[
       "When a syndicate is transferable, each member may transfer their ownership in the syndicate to any other address. If an allowlist is used, this new address must also be on the allowlist.",
       "Here, you’re setting permissions only for the member. If you chose to make this syndicate modifiable in the previous step, as a manager, you’ll still be able to modify or transfer ownership even if you choose to make the syndicate “not transferable” in this step.",
