b120f5c7dc9e19f012ae0fc9ca65334ea81c73ac
Rachael Njeri <njerirachael0@gmail.com>
2021-08-18 09:00:02 +0300

    [ENG-1244]Inject web3 into end-to-end tests (#398)
    * Configure cypress using truffle-privatekey-provider
    
    Signed-off-by: Rachael Njeri <njerirachael0@gmail.com>
    
    * Update using HDWallet provider
    
    Signed-off-by: Rachael Njeri <njerirachael0@gmail.com>

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,19 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
+import Web3 from "web3";
+import HDWalletProvider from "@truffle/hdwallet-provider";
+
+describe("Landing page", () => {
+  it("Should have a the Join the Revolution text", () => {
+    cy.on("window:before:load", (win) => {
+      const provider = new HDWalletProvider({
+        privateKeys: Cypress.env("ETH_PRIVATE_KEY"),
+        mnemonic: Cypress.env("mnemonicPhrase"),
+        providerOrUrl: Cypress.env("providerOrUrl"),
+        numberOfAddresses: 1,
+        derivationPath: "m/44'/60'/0'/0/0",
+      });
+      win.web3 = new Web3(provider);
+    });
+    cy.visit("http://localhost:3000");
+    cy.get(`#${CSS.escape("__next")}`).contains("Join the Revolution");
   });
 });
