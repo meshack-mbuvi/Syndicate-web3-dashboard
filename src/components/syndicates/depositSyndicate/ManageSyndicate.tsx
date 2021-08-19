e8378ca6a237c25806f920abafdfb587ec644df2
Justin Ndwiga <ndwigajustin@gmail.com>
2021-08-19 14:14:20 +0300

    check mngr address opposed to syn address

--- src/components/syndicates/depositSyndicate/ManageSyndicate.tsx
+++ src/components/syndicates/depositSyndicate/ManageSyndicate.tsx
@@ -23,7 +23,7 @@ const ManageSyndicate = ({ syndicateAddress }: IProps): JSX.Element => {
     if (syndicate?.depositsEnabled) {
       url = "deposit";
       urlText = "Go To Deposit Page";
-    } else if(syndicate?.distributing){
+    } else {
       url = "withdraw";
       urlText = "Go To Withdrawal Page";
     }
