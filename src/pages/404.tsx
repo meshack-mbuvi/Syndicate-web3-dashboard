9972a2dcfde1272194bd0e81e93c47e08e91a212
zandialex <zandialex+2@gmail.com>
2021-08-27 00:12:21 -0700

    [ENG-1394] Update 404 page (#433)
    * Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Router from "next/router";
+import PrimaryButton from "@/components/buttons";
 
 import Layout from "src/components/layout";
 import SEO from "src/components/seo";
@@ -7,13 +9,24 @@ function NotFoundPage() {
   return (
     <Layout>
       <SEO
-        keywords={[`next`, `tailwind`, `react`, `tailwindcss`]}
-        title="404: Not found"
+        keywords={[`syndicate`, `crypto`, `invest`, `fund`, `social`, `ethereum`]}
+        title="404: Not Found"
       />
-      <div>
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Looks like this page is a ghost that got abducted by aliens...
-        </h2>
+      <div className="w-full" style={{height: "calc(100vh - 300px)"}}>
+        <div className="vertically-center container mx-auto">
+          <div className="w-full sm:w-8/12 lg:w-5/12">
+            <h2 className="tagline mb-4">404 / Not Found</h2>
+            <h1 className="font-whyte text-3xl mb-3">There's nothing here yet</h1>
+            <p className="mb-8 text-gray-3">Double check the address or wait a minute and then try refreshing this page. Syndicate works on the blockchain, so things can sometimes take an extra moment to process.</p>
+            <PrimaryButton 
+              customClasses="px-12 py-3.5 rounded-lg focus:outline-none border-none bg-gray-manatee bg-opacity-30 text-white" 
+              onClick={() => {Router.push("/syndicates")}}
+            >
+                <img className="inline w-4 mr-3 -ml-1" src="/images/leftArrow.svg" alt="Back Arrow"/>
+                Back
+            </PrimaryButton>
+          </div>
+        </div>
       </div>
     </Layout>
   );
