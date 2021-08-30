5abba4fd637674b5a4a26e3bd67e8d4050168c5c
zandialex <zandialex+2@gmail.com>
2021-08-30 16:10:09 -0700

    Nav bar nav items are more spaced apart (#438)

--- src/components/navigation/header/navbarItems/index.tsx
+++ src/components/navigation/header/navbarItems/index.tsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export const NavBarNavItem = ({ url, urlText, customClasses = "" }) => {
   return (
     <Link href={`${url}`}>
-      <a className={`${customClasses} text-sm sm:text-base text-white leading-7 font-light mr-3 lg:mr-6 active:text-blue hover:text-blue transition-all`}>
+      <a className={`${customClasses} text-sm sm:text-base text-white leading-7 font-light mr-3 md:mr-6 lg:mr-9 active:text-blue hover:text-blue transition-all`}>
         {urlText}
       </a>
     </Link>
