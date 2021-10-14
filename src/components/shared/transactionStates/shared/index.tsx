c8641af46d8cbe371a310e0d09d88fe7d547dc9c
Jillo Abdullahi <jillo.abdulla@gmail.com>
2021-10-14 17:01:20 +0300

    update transaction states modal (#546)

--- src/components/shared/transactionStates/shared/index.tsx
+++ src/components/shared/transactionStates/shared/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 export enum ModalStyle {
   DARK = "dark",
@@ -8,14 +9,19 @@ interface Props {
   show: boolean;
   children: JSX.Element;
   width?: string;
-  modalStyle?:string;
+  modalStyle?: string;
 }
 /**
  * This is a modal that shows different transaction states
  * @returns an html node in a form of a modal
  */
 export const StateModal = (props: Props) => {
-  const { children, show, width = "w-1/3", modalStyle = ModalStyle.LIGHT } = props;
+  const {
+    children,
+    show,
+    width = "w-1/3",
+    modalStyle = ModalStyle.LIGHT,
+  } = props;
   const bgColor = `${modalStyle === ModalStyle.LIGHT && "bg-white"} ${
     modalStyle === ModalStyle.DARK && "bg-gray-4"
   }`;
@@ -26,21 +32,75 @@ export const StateModal = (props: Props) => {
   return (
     <>
       {show ? (
-        <div className="fixed z-50 inset-0 overflow-y-auto mt-6">
+        <div className="fixed z-100 inset-0 overflow-y-auto mt-6">
           <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-            <div className="absolute inset-0  bg-gray-9 opacity-95"></div>
+            <div className="absolute inset-0  bg-gray-9 bg-opacity-60"></div>
           </div>
-          <div className={`flex items-end justify-center ${textColor} min-h-screen sm:pt-4 sm:px-4 pb-20 text-center sm:block sm:p-0`}>
+          <div
+            className={`flex items-end justify-center ${textColor} min-h-screen sm:pt-4 sm:px-4 pb-20 text-center sm:block sm:p-0`}
+          >
             <div
               className={`inline-block align-bottom ${bgColor} rounded-lg sm:my-28 sm:p-6 text-left overflow-hidden shadow-xl transform transition-all max-w-868 ${width}`}
               role="dialog"
               aria-modal="true"
-              aria-labelledby="modal-headline">
+              aria-labelledby="modal-headline"
+            >
               <div className="mx-4">{children}</div>
             </div>
           </div>
         </div>
       ) : null}
+
+      <Transition.Root show={show} as={Fragment}>
+        <Dialog
+          className={`fixed z-50 w-screen h-screen overflow-y-scroll no-scroll-bar justify-center align-middle py-auto inset-0 text-center`}
+          onClose={() => null}
+          open={show}
+        >
+          <div
+            className={`flex items-center my-auto justify-center text-center ${textColor} sm:px-4 text-center sm:block sm:p-0`}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-60 transition-opacity" />
+            </Transition.Child>
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div
+                className={`overflow-y-scroll no-scroll-bar md:my-14 align-middle mx-auto inline-block max-h-screen ${
+                  bgColor ? bgColor : ""
+                } rounded-2xl text-left shadow-xl transform transition-all max-w-868 ${width}`}
+                role="dialog"
+                aria-modal="true"
+                aria-labelledby="modal-headline"
+              >
+                <div className="m-4">{children}</div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 };
