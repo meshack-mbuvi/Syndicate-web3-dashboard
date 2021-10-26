c010e7b9302d839200c761425b38cf62a80001fa
Justin Ndwiga <ndwigajustin@gmail.com>
2021-10-26 17:02:04 +0300

    fix spacing in deposit field

--- src/components/inputs/autoGrowInput/index.tsx
+++ src/components/inputs/autoGrowInput/index.tsx
@@ -16,22 +16,22 @@ const AutoGrowInputField: React.FC<AutoGrowInputField> = ({
   ...rest
 }) => {
   const [width, setWidth] = useState(50);
-  const [dynamicFontSize, setDynamicFontSize] = useState(56);
+  const [dynamicFontSize, setDynamicFontSize] = useState(48);
   const span = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
     // add 16 for 1rem spacing
-    setWidth(span.current.offsetWidth + 16);
+    setWidth(span.current.offsetWidth + 4);
     // reset font size if no value
     if (!value) {
-      setDynamicFontSize(56);
+      setDynamicFontSize(48);
     }
   }, [value]);
 
   useEffect(() => {
     if (span.current.offsetWidth > width) {
       setDynamicFontSize(() => {
-        const tempSize = 56 - span.current.offsetWidth * 0.15;
+        const tempSize = 48 - span.current.offsetWidth * 0.1;
         // set the minimum font to 16
         return tempSize > 16 ? tempSize : 16;
       });
@@ -39,11 +39,10 @@ const AutoGrowInputField: React.FC<AutoGrowInputField> = ({
   }, [dynamicFontSize, width, value]);
 
   const { width: windowWidth } = useWindowSize();
-
   return (
     <div className="h-full text-5xl">
       <span
-        className="absolute opacity-0 whitespace-pre px-1"
+        className="absolute opacity-0 whitespace-pre"
         style={{ zIndex: -100 }}
         ref={span}
       >
@@ -53,12 +52,12 @@ const AutoGrowInputField: React.FC<AutoGrowInputField> = ({
         {...rest}
         value={value}
         thousandSeparator={true}
-        className={`bg-transparent border-none outline-none h-full p-0 px-1 text-5xl font-whyte focus:outline-none focus:border-none focus:ring-0 ${
+        className={`bg-transparent border-none outline-none h-full p-0 text-5xl font-whyte focus:outline-none focus:border-none focus:ring-0 ${
           hasError ? "text-red-semantic" : ""
         } ${value ? "text-white" : "text-gray-syn4"}`}
         style={{
           width,
-          maxWidth: windowWidth < 1200 && windowWidth < 860 ? 120 : 150,
+          maxWidth: windowWidth < 1200 && windowWidth < 860 ? 100 : 110,
           fontSize: dynamicFontSize,
         }}
         placeholder={placeholder}
