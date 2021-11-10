7a8e8a8389cb80447fdb9e47936fdd65cbb1260f
Jillo Abdullahi <jillo.abdulla@gmail.com>
2021-11-10 16:05:10 +0300

    [ENG-2342] Implement assets section QA feedback (#666)
    * implement all assets section qa feedback
    
    * fix type validation issue
    
    * update how assets tabs are shown

--- src/containers/layoutWithSyndicateDetails/assets/constants.ts
+++ src/containers/layoutWithSyndicateDetails/assets/constants.ts
@@ -1,17 +1,2 @@
 export const tokenTableColumns = ["Token", "Balance", "Value"];
 
-export const assetsFilterOptions = [
-  {
-    label: "All Assets",
-    value: "all",
-  },
-  {
-    label: "Tokens",
-    value: "tokens",
-  },
-  {
-    label: "Collectibles",
-    value: "collectibles",
-  },
-];
-
