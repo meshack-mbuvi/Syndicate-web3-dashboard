fe13db7472f45c6057e0a7c2ddcc040e0ddbf179
ibremseth <idbremseth@gmail.com>
2022-01-14 12:57:20 -0500

    Changed api URL to env variable (#935)

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
-import { isDev } from "@/utils/environment";
-
-const baseURL = isDev
-  ? "https://us-central1-secret-shell-323016.cloudfunctions.net/api-proxy"
-  : "https://us-central1-syndicate-production.cloudfunctions.net/api-proxy";
 
 export const proxyGet = async (path: string, params: any) => {
-    return await axios.get(`${baseURL}/${path}`, {
+    return await axios.get(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/${path}`, {
         params,
       });
 };
