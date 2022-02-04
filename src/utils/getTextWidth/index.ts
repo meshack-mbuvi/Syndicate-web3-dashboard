f356c57cba4e24a00417a1056c539b39514cb083
Alweezy <alvinmutisya@gmail.com>
2022-02-04 21:30:15 +0300

    Cach ssr document error

--- src/utils/getTextWidth/index.ts
+++ src/utils/getTextWidth/index.ts
@@ -1,5 +1,6 @@
 // Use canvas to determine text width
 export const getTextWidth = (text) => {
+  if (typeof document === 'undefined') return
   const canvas = document?.createElement("canvas");
   const context = canvas.getContext("2d");
   context.font = getComputedStyle(document.body).font;
