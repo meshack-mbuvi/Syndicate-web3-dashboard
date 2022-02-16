d28997597b341c24cf78123bc2142ec76adb0839
Justin Ndwiga <ndwigajustin@gmail.com>
2022-02-16 18:16:53 +0300

    [ENG-3200] Activity modal responsive
    * activity modal responsive
    
    * activity modal extra small screens

--- src/containers/layoutWithSyndicateDetails/activity/shared/TransactionDetails/index.tsx
+++ src/containers/layoutWithSyndicateDetails/activity/shared/TransactionDetails/index.tsx
@@ -9,6 +9,7 @@ import {
   RoundCategory,
   TransactionCategory,
 } from "@/state/erc20transactions/types";
+import useWindowSize from "@/hooks/useWindowSize";
 
 type Transaction = "outgoing" | "incoming";
 
@@ -80,50 +81,65 @@ const TransactionDetails: React.FC<ITransactionDetails> = ({
       ).length !== 0
     );
   };
+  const { width } = useWindowSize();
 
   return (
     <>
       {category !== "OFF_CHAIN_INVESTMENT" ? (
-        <div className="flex items-center">
-          {tokenSymbol.toLowerCase() !== "usd" && (
-            <>
-              {tokenLogo ? (
-                <Image src={tokenLogo} height={24} width={24} />
-              ) : (
-                <GradientAvatar name={tokenName} size={"w-6 h-6"} />
-              )}
-            </>
-          )}
-          <div className={`flex ml-2 ${onModal ? "text-2xl" : "text-base"}`}>
-            {addGrayToDecimalInput(floatedNumberWithCommas(amount))}&nbsp;
-            {tokenSymbol}
+        <div className={`flex items-center ${width < 400 ? "flex-col" : ""}`}>
+          <div className="flex">
+            {tokenSymbol.toLowerCase() !== "usd" && (
+              <>
+                {tokenLogo ? (
+                  <Image src={tokenLogo} height={24} width={24} />
+                ) : (
+                  <GradientAvatar name={tokenName} size={"w-6 h-6"} />
+                )}
+              </>
+            )}
+            <div
+              className={`flex ml-2 ${
+                onModal ? "sm:text-2xl text-base" : "text-base"
+              }`}
+            >
+              {addGrayToDecimalInput(floatedNumberWithCommas(amount))}&nbsp;
+              {tokenSymbol}
+            </div>
           </div>
           <p className={`text-gray-syn4 ${onModal ? "mx-4" : "mx-3"}`}>
             {getTransactionText(transactionType, onModal)}
           </p>
-          {!onModal &&
-          transactionType === "incoming" &&
-          isTransactionAnnotated ? (
-            <>
-              {AddressIsMember(address) && (
-                <div className="mx-2 flex items-center">
-                  <Image src={"/images/User_Icon.svg"} height={24} width={24} />
-                </div>
-              )}
-            </>
-          ) : null}
-          {onModal && category === "DEPOSIT" ? (
-            <div className="mr-2 flex items-center">
-              <Image src={"/images/User_Icon.svg"} height={24} width={24} />
+          <div className="flex">
+            {!onModal &&
+            transactionType === "incoming" &&
+            isTransactionAnnotated ? (
+              <>
+                {AddressIsMember(address) && (
+                  <div className="mx-2 flex items-center">
+                    <Image
+                      src={"/images/User_Icon.svg"}
+                      height={24}
+                      width={24}
+                    />
+                  </div>
+                )}
+              </>
+            ) : null}
+            {onModal && category === "DEPOSIT" ? (
+              <div className="mr-2 flex items-center">
+                <Image src={"/images/User_Icon.svg"} height={24} width={24} />
+              </div>
+            ) : null}
+            <div
+              className={`${onModal ? "sm:text-2xl text-base" : "text-base"}`}
+            >
+              {companyName
+                ? companyName
+                : !web3.utils.isAddress(address)
+                ? address
+                : formatAddress(address, 6, 4)}
             </div>
-          ) : null}
-          <p className={`${onModal ? "text-2xl" : "text-base"}`}>
-            {companyName
-              ? companyName
-              : !web3.utils.isAddress(address)
-              ? address
-              : formatAddress(address, 6, 4)}
-          </p>
+          </div>
         </div>
       ) : (
         <div className="flex flex-col items-center">
