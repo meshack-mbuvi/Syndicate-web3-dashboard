8d09c194f1fdaa1be3b024cc0c597bb172c9e3d2
daniel kipkemei <dann.professional@gmail.com>
2022-03-22 19:51:01 +0300

    add back connected With

--- src/components/navigation/header/wallet/accountMenuDropdown.tsx
+++ src/components/navigation/header/wallet/accountMenuDropdown.tsx
@@ -83,6 +83,9 @@ const AddressMenuDropDown: FC<IAddressMenuDropDown> = ({
                       {formatAddress(account.substring(2), 6, 6)}
                     </p>
                   </div>
+                  <div className="flex justify-between mt-2">
+                    {renderConnectedWith(providerName)}
+                  </div>
                 </div>
 
                 <div
