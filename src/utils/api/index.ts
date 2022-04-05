f10870664da1c72975f37f2ea28bc1636d9ac4a6
Anthony DiPadova <tdipadova3rd@gmail.com>
2022-04-05 12:22:14 -0700

    Fix axios return types

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-export const proxyGet = async (path: string, params: any) => {
+export async function proxyGet(path: string, params: unknown): Promise<AxiosResponse> {
   return await axios.get(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/${path}`, {
     params
   });
-};
+}
