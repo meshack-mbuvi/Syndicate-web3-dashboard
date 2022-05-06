5d780f36405cbc2280c67d9fa168104df6b8c482
zandialex <zandialex+2@gmail.com>
2022-05-05 20:41:50 -0400

    Update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -60,10 +60,15 @@ const App = ({ Component, pageProps, apollo }) => {
             <CreateInvestmentClubProvider>
               <Head>
                 <title>Home | Syndicate Dashboard</title>
-                <link rel="shortcut icon" href="/images/logo.svg" />
-
+                {/* Safari favicon */}
+                <link rel="icon" href="/favicon.png" sizes="any" />
+                {/* Favicon */}
+                <link
+                  rel="icon"
+                  href="/favicon.svg"
+                  type="image/svg+xml"
+                ></link>{' '}
                 <FontsPreloader />
-
                 <meta
                   name="viewport"
                   content="width=device-width, initial-scale=1, shrink-to-fit=no"
