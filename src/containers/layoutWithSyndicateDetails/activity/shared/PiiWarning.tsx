9b2f1fb61c360963b0a7e7375c32a2f08a32725f
zandialex <zandialex+2@gmail.com>
2022-05-16 15:19:34 -0400

    Update PII warning

--- src/containers/layoutWithSyndicateDetails/activity/shared/PiiWarning.tsx
+++ src/containers/layoutWithSyndicateDetails/activity/shared/PiiWarning.tsx
@@ -4,9 +4,8 @@ const PiiWarning: React.FC = () => {
   return (
     <div className="w-full py-4 px-5 mt-6 rounded-lg font-whyte text-yellow-saffron bg-brown-dark">
       <p className="text-base">
-        To comply with privacy regulations, please do not enter Personal
-        Identifiable Information (PII) of any individual involved in this club
-        or investment.
+        This information may be publicly visible off-chain, so we do not
+        recommend storing sensitive information.
       </p>
     </div>
   );
