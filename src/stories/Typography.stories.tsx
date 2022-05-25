23e5f5861b4b78c84a92a26e5db3de70b553d51f
zandialex <zandialex+2@gmail.com>
2022-05-24 23:35:42 -0400

    Update Typography story to use new styles

--- src/stories/Typography.stories.tsx
+++ src/stories/Typography.stories.tsx
@@ -1,3 +1,18 @@
+import {
+  E1,
+  E2,
+  H1,
+  H2,
+  H3,
+  H4,
+  L1,
+  L2,
+  T1,
+  T2,
+  T3,
+  T4,
+  T5
+} from '@/components/typography';
 import React from 'react';
 
 export default {
@@ -6,12 +21,43 @@ export default {
 
 const Template = (args) => (
   <div className="space-y-4 text-white" {...args}>
-    <h1>{args.h1}</h1>
-    <h2>{args.h2}</h2>
-    <h3>{args.h3}</h3>
-    <h4>{args.h4}</h4>
-    <p>{args.body}</p>
-    <p className="text-sm">{args.smallBody}</p>
+    <div className="flex space-x-8">
+      <div className="w-1/2 space-y-4">
+        <T1>T1, Semibold, 88</T1>
+        <T2>T2, Semibold, 72</T2>
+        <T3>T3, Semibold, 64</T3>
+        <T4>T4, Semibold, 56</T4>
+        <T5>T5, Semibold, 48</T5>
+      </div>
+      <div className="w-1/2 space-y-4">
+        <T1 medium>T1, Medium, 88</T1>
+        <T2 medium>T2, Medium, 72</T2>
+        <T3 medium>T3, Medium, 64</T3>
+        <T4 medium>T4, Medium, 56</T4>
+        <T5 medium>T5, Medium, 48</T5>
+      </div>
+    </div>
+    <hr className="border-gray-syn6" />
+    <div className="flex space-x-8">
+      <div className="w-1/2 space-y-3">
+        <H1>H1, Medium, 40</H1>
+        <H2>H2, Medium, 32</H2>
+        <H3>H3, Medium, 24</H3>
+        <H4>H4, Medium, 20</H4>
+      </div>
+      <div className="w-1/2 space-y-3">
+        <H1 regular>H1, Regular, 40</H1>
+        <H2 regular>H2, Regular, 32</H2>
+        <H3 regular>H3, Regular, 24</H3>
+        <H4 regular>H4, Regular, 20</H4>
+      </div>
+    </div>
+    <hr className="border-gray-syn6" />
+    <L1>{args.label1}</L1>
+    <L2>{args.label2}</L2>
+    <hr className="border-gray-syn6" />
+    <E1>E1, Regular, 16</E1>
+    <E2>E2, Regular, 14</E2>
   </div>
 );
 
@@ -22,5 +68,7 @@ Default.args = {
   h3: 'H3 Regular',
   h4: 'H4 Bold',
   body: 'Body, Regular',
-  smallBody: 'Small Body, Regular'
+  smallBody: 'Small Body, Regular',
+  label1: 'L1, bold, 16',
+  label2: 'L2, bold, 14'
 };
