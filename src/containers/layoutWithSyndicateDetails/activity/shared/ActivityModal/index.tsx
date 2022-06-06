919fdb0ba4173446d15dfcee6a3d2fb14f911bd3
jillo-abdullahi <jillo.abdulla@gmail.com>
2022-06-06 16:26:48 +0300

    fix intermediate content shown on modal close

--- src/containers/layoutWithSyndicateDetails/activity/shared/ActivityModal/index.tsx
+++ src/containers/layoutWithSyndicateDetails/activity/shared/ActivityModal/index.tsx
@@ -9,7 +9,6 @@ import { MEMBER_SIGNED_QUERY } from '@/graphql/queries';
 import { useIsClubOwner } from '@/hooks/useClubOwner';
 import { useDemoMode } from '@/hooks/useDemoMode';
 import { AppState } from '@/state';
-import { isDev } from '@/utils/environment';
 import { useMutation, useQuery } from '@apollo/client';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
@@ -258,180 +257,182 @@ const ActivityModal: React.FC<IActivityModal> = ({
       overflowXScroll={false}
       maxHeight={false}
     >
-      <div className="relative">
-        {isDemoMode && <div className="absolute inset-0 z-10" />}
-        <div
-          className={`flex rounded-t-2xl items-center flex-col relative py-10 px-5 ${adaptiveBackground} last:rounded-b-2xl`}
-        >
+      {showModal && (
+        <div className="relative">
+          {isDemoMode && <div className="absolute inset-0 z-10" />}
           <div
-            onMouseLeave={() => toggleDropDown(true)}
-            onMouseEnter={() => toggleDropDown(false)}
+            className={`flex rounded-t-2xl items-center flex-col relative py-10 px-5 ${adaptiveBackground} last:rounded-b-2xl`}
           >
-            <div className="mb-8">
-              <CategoryPill
-                category={category}
-                outgoing={
-                  transactionInfo?.isOutgoingTransaction
-                    ? transactionInfo.isOutgoingTransaction
-                    : false
-                }
-                readonly={readOnly}
-                changeAdaptiveBackground={changeAdaptiveBackground}
-                renderedInModal={true}
-                refetchTransactions={refetchTransactions}
-                transactionHash={transactionInfo?.transactionHash}
-                disableDropDown={disableDropDown}
-              />
+            <div
+              onMouseLeave={() => toggleDropDown(true)}
+              onMouseEnter={() => toggleDropDown(false)}
+            >
+              <div className="mb-8">
+                <CategoryPill
+                  category={category}
+                  outgoing={
+                    transactionInfo?.isOutgoingTransaction
+                      ? transactionInfo.isOutgoingTransaction
+                      : false
+                  }
+                  readonly={readOnly}
+                  changeAdaptiveBackground={changeAdaptiveBackground}
+                  renderedInModal={true}
+                  refetchTransactions={refetchTransactions}
+                  transactionHash={transactionInfo?.transactionHash}
+                  disableDropDown={disableDropDown}
+                />
+              </div>
+            </div>
+            <div className="items-center flex flex-col">
+              {transactionInfo && Object.keys(transactionInfo).length && (
+                <TransactionDetails
+                  tokenLogo={tokenLogo}
+                  tokenSymbol={
+                    category === 'INVESTMENT' ||
+                    category === 'OFF_CHAIN_INVESTMENT'
+                      ? 'USD'
+                      : tokenSymbol
+                  }
+                  tokenName={tokenName}
+                  transactionType={
+                    transactionInfo.isOutgoingTransaction
+                      ? 'outgoing'
+                      : 'incoming'
+                  }
+                  isTransactionAnnotated={false}
+                  amount={
+                    category === 'INVESTMENT' ||
+                    category === 'OFF_CHAIN_INVESTMENT'
+                      ? metadata?.postMoneyValuation
+                      : amount
+                  }
+                  address={
+                    transactionInfo.isOutgoingTransaction
+                      ? transactionInfo.to
+                      : transactionInfo.from
+                  }
+                  onModal={true}
+                  category={category}
+                  companyName={metadata?.companyName}
+                  round={metadata?.roundCategory}
+                />
+              )}
+
+              {category !== 'OFF_CHAIN_INVESTMENT' ? (
+                <div className="text-gray-lightManatee text-sm mt-6 flex items-center justify-center">
+                  <a
+                    className="flex cursor-pointer items-center"
+                    href={`${blockExplorerLink}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img
+                      className="pr-2"
+                      src={`/images/actionIcons/checkMark.svg`}
+                      alt=""
+                    />{' '}
+                    Completed on {timestamp}
+                    <OpenExternalLinkIcon className="text-gray-syn4 ml-2 w-3 h-3" />
+                  </a>
+                </div>
+              ) : null}
             </div>
           </div>
-          <div className="items-center flex flex-col">
-            {transactionInfo && Object.keys(transactionInfo).length && (
-              <TransactionDetails
-                tokenLogo={tokenLogo}
-                tokenSymbol={
-                  category === 'INVESTMENT' ||
-                  category === 'OFF_CHAIN_INVESTMENT'
-                    ? 'USD'
-                    : tokenSymbol
-                }
-                tokenName={tokenName}
-                transactionType={
-                  transactionInfo.isOutgoingTransaction
-                    ? 'outgoing'
-                    : 'incoming'
-                }
-                isTransactionAnnotated={false}
-                amount={
-                  category === 'INVESTMENT' ||
-                  category === 'OFF_CHAIN_INVESTMENT'
-                    ? metadata?.postMoneyValuation
-                    : amount
-                }
-                address={
-                  transactionInfo.isOutgoingTransaction
-                    ? transactionInfo.to
-                    : transactionInfo.from
-                }
-                onModal={true}
-                category={category}
-                companyName={metadata?.companyName}
-                round={metadata?.roundCategory}
-              />
-            )}
 
-            {category !== 'OFF_CHAIN_INVESTMENT' ? (
-              <div className="text-gray-lightManatee text-sm mt-6 flex items-center justify-center">
-                <a
-                  className="flex cursor-pointer items-center"
-                  href={`${blockExplorerLink}`}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    className="pr-2"
-                    src={`/images/actionIcons/checkMark.svg`}
-                    alt=""
-                  />{' '}
-                  Completed on {timestamp}
-                  <OpenExternalLinkIcon className="text-gray-syn4 ml-2 w-3 h-3" />
-                </a>
-              </div>
-            ) : null}
-          </div>
-        </div>
-
-        {/* Show this component only when manager has not marked member signature status 
+          {/* Show this component only when manager has not marked member signature status 
         
         Adding essential check for isManager. Members should not see this*/}
 
-        {!data?.Financial_memberSigned &&
-          !loading &&
-          category === 'DEPOSIT' &&
-          isManager && (
-            <div className="flex flex-col space-y-6 py-6 px-5">
-              <div className="bg-gray-syn7 px-5 py-4 space-y-2 rounded-xl">
-                <p className="text-gray-syn4 leading-6">
-                  Has this member signed the associated legal agreements?
-                </p>
-                <button
-                  className="text-blue"
-                  onClick={handleSetMemberHasSigned}
-                >
-                  Yes, mark as signed
-                </button>
+          {!data?.Financial_memberSigned &&
+            !loading &&
+            category === 'DEPOSIT' &&
+            isManager && (
+              <div className="flex flex-col space-y-6 py-6 px-5">
+                <div className="bg-gray-syn7 px-5 py-4 space-y-2 rounded-xl">
+                  <p className="text-gray-syn4 leading-6">
+                    Has this member signed the associated legal agreements?
+                  </p>
+                  <button
+                    className="text-blue"
+                    onClick={handleSetMemberHasSigned}
+                  >
+                    Yes, mark as signed
+                  </button>
+                </div>
               </div>
+            )}
+
+          {/* Note and details section */}
+          {category === 'DEPOSIT' ||
+          category === 'UNCATEGORISED' ||
+          category === null ||
+          (!isManager && !note && !showDetailSection) ? null : (
+            <div className="flex flex-col space-y-6 py-6 px-5">
+              {/* note */}
+              {!showNote && isManager ? (
+                <button
+                  className="flex items-center px-5 py-4 text-base text-gray-lightManatee bg-blue-darkGunMetal rounded-1.5lg leading-6 cursor-pointer"
+                  onClick={() => setShowNote(true)}
+                >
+                  <Image
+                    src={`/images/actionIcons/plus-sign.svg`}
+                    height={16}
+                    width={16}
+                  />
+                  <span className="ml-2">Add note</span>
+                </button>
+              ) : (
+                <div className="">
+                  {!note && !isManager ? null : (
+                    <ActivityNote
+                      saveTransactionNote={saveTransactionNote}
+                      setShowNote={setShowNote}
+                    />
+                  )}
+                </div>
+              )}
+
+              {/* details */}
+              {(category === 'INVESTMENT' ||
+                category === 'OFF_CHAIN_INVESTMENT') && (
+                <div>
+                  {/* Checks if the stored investment details has empty values */}
+                  {!showDetailSection && !editMode && isManager && (
+                    <button
+                      className="w-full flex items-center px-5 py-4 text-base text-gray-lightManatee bg-blue-darkGunMetal rounded-1.5lg leading-6 cursor-pointer"
+                      onClick={() => handleAddDetails()}
+                    >
+                      <Image
+                        src={`/images/actionIcons/plus-sign.svg`}
+                        height={16}
+                        width={16}
+                      />
+                      <span className="ml-2">Add details</span>
+                    </button>
+                  )}
+                  {/* implement Details edit and view mode here */}
+                  {showDetailSection || editMode ? (
+                    <InvestmentDetailsModal
+                      showModal={showTransactionDetails}
+                      editMode={editMode}
+                      readonly={true}
+                      onClick={handleClick}
+                      storedInvestmentDetails={storedInvestmentDetails}
+                      transactionId={hash}
+                      setStoredInvestmentDetails={setStoredInvestmentDetails}
+                      isManager={isManager}
+                      onSuccessfulAnnotation={() => {
+                        refetchTransactions();
+                      }}
+                    />
+                  ) : null}
+                </div>
+              )}
             </div>
           )}
-
-        {/* Note and details section */}
-        {category === 'DEPOSIT' ||
-        category === 'UNCATEGORISED' ||
-        category === null ||
-        (!isManager && !note && !showDetailSection) ? null : (
-          <div className="flex flex-col space-y-6 py-6 px-5">
-            {/* note */}
-            {!showNote && isManager ? (
-              <button
-                className="flex items-center px-5 py-4 text-base text-gray-lightManatee bg-blue-darkGunMetal rounded-1.5lg leading-6 cursor-pointer"
-                onClick={() => setShowNote(true)}
-              >
-                <Image
-                  src={`/images/actionIcons/plus-sign.svg`}
-                  height={16}
-                  width={16}
-                />
-                <span className="ml-2">Add note</span>
-              </button>
-            ) : (
-              <div className="">
-                {!note && !isManager ? null : (
-                  <ActivityNote
-                    saveTransactionNote={saveTransactionNote}
-                    setShowNote={setShowNote}
-                  />
-                )}
-              </div>
-            )}
-
-            {/* details */}
-            {(category === 'INVESTMENT' ||
-              category === 'OFF_CHAIN_INVESTMENT') && (
-              <div>
-                {/* Checks if the stored investment details has empty values */}
-                {!showDetailSection && !editMode && isManager && (
-                  <button
-                    className="w-full flex items-center px-5 py-4 text-base text-gray-lightManatee bg-blue-darkGunMetal rounded-1.5lg leading-6 cursor-pointer"
-                    onClick={() => handleAddDetails()}
-                  >
-                    <Image
-                      src={`/images/actionIcons/plus-sign.svg`}
-                      height={16}
-                      width={16}
-                    />
-                    <span className="ml-2">Add details</span>
-                  </button>
-                )}
-                {/* implement Details edit and view mode here */}
-                {showDetailSection || editMode ? (
-                  <InvestmentDetailsModal
-                    showModal={showTransactionDetails}
-                    editMode={editMode}
-                    readonly={true}
-                    onClick={handleClick}
-                    storedInvestmentDetails={storedInvestmentDetails}
-                    transactionId={hash}
-                    setStoredInvestmentDetails={setStoredInvestmentDetails}
-                    isManager={isManager}
-                    onSuccessfulAnnotation={() => {
-                      refetchTransactions();
-                    }}
-                  />
-                ) : null}
-              </div>
-            )}
-          </div>
-        )}
-      </div>
+        </div>
+      )}
     </Modal>
   );
 };
