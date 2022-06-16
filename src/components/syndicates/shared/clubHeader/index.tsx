4bc29a031b8ed6b6fd5e992e7230bf9ecd6ea13b
jillo-abdullahi <jillo.abdulla@gmail.com>
2022-06-16 18:33:34 +0300

    use fake owner address in demo mode

--- src/components/syndicates/shared/clubHeader/index.tsx
+++ src/components/syndicates/shared/clubHeader/index.tsx
@@ -1,7 +1,6 @@
 import { CopyToClipboardIcon } from '@/components/iconWrappers';
 import { SkeletonLoader } from '@/components/skeletonLoader';
 import { H1 } from '@/components/typography';
-import { useDemoMode } from '@/hooks/useDemoMode';
 import { getTextWidth } from '@/utils/getTextWidth';
 import React, { useEffect, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
@@ -33,7 +32,6 @@ export const ClubHeader: React.FC<{
   const [nameWidth, setNameWidth] = useState(0);
 
   const [showActionIcons, setShowActionIcons] = useState<boolean>(false);
-  const isDemoMode = useDemoMode();
 
   // state to handle copying of the syndicate address to clipboard.
   const [showAddressCopyState, setShowAddressCopyState] =
@@ -67,7 +65,7 @@ export const ClubHeader: React.FC<{
         {/* Syndicate name, symbol and action buttons  */}
         <div
           className="flex justify-start items-center"
-          onMouseEnter={() => !isDemoMode && setShowActionIcons(true)}
+          onMouseEnter={() => setShowActionIcons(true)}
           onMouseLeave={() => setShowActionIcons(false)}
         >
           {loading ? (
