63091609fc92c8ce6069744ecb7894cbb28dd0bd
daniel kipkemei <dann.professional@gmail.com>
2022-06-28 17:43:13 +0300

    fix empty web3

--- src/components/syndicates/syndicateDetails.tsx
+++ src/components/syndicates/syndicateDetails.tsx
@@ -13,6 +13,7 @@ import { floatedNumberWithCommas } from '@/utils/formattedNumbers';
 import { mockDepositERC20Token } from '@/utils/mockdata';
 import { NetworkStatus, useQuery } from '@apollo/client';
 import abi from 'human-standard-token-abi';
+import { isEmpty } from 'lodash';
 import { useRouter } from 'next/router';
 import React, { FC, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -181,7 +182,7 @@ const SyndicateDetails: FC<{ managerSettingsOpen: boolean }> = ({
 
   // get and set current token details
   useEffect(() => {
-    if (!nativeDepositToken && depositToken && web3) {
+    if (!nativeDepositToken && depositToken && !isEmpty(web3)) {
       // set up token contract
       const tokenContract = new web3.eth.Contract(abi, depositToken);
       setDepositTokenContract(tokenContract);
