d54cc681feb672923eb5fe48309b850bf2252b54
Meshack Mbuvi <meshmbuvi@gmail.com>
2022-07-15 13:11:25 +0300

    Show tooltip to explain why ntfs don't show up. (#1446)
    * Show tooltip to explain why ntfs don't show up.
    
    * Fix tooltip text color on mobile.

--- src/containers/distribute/TokenSelector/index.tsx
+++ src/containers/distribute/TokenSelector/index.tsx
@@ -2,8 +2,10 @@ import { AssetList } from '@/components/distributions/assetList';
 import { SkeletonLoader } from '@/components/skeletonLoader';
 import { B3, B4 } from '@/components/typography';
 import { AppState } from '@/state';
+import Image from 'next/image';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import ReactTooltip from 'react-tooltip';
 import DistributionHeader from '../DistributionHeader';
 
 const TokenSelector: React.FC<{
@@ -25,18 +27,43 @@ const TokenSelector: React.FC<{
         titleText="What would you like to distribute?"
         subTitleText={`Assets are distributed in proportion to members’ ownership of ${symbol} tokens.`}
       />
-      <div>
-        <div className="hidden md:block">
-          <B3 extraClasses="flex text-gray-syn5 mt-2">
-            <img src="/images/question-gray5.svg" alt="" className="mr-2" />
-            Why don’t NFTs show up here?
-          </B3>
-        </div>
-        <div className="md:hidden">
-          <B4 extraClasses="flex text-gray-syn5 mt-2">
-            <img src="/images/question-gray5.svg" alt="" className="mr-2" />
-            Why don’t NFTs show up here?
-          </B4>
+      <div className="mt-1">
+        <div className="flex">
+          <div
+            data-tip
+            data-for="why-nft-don't-show"
+            id="why-nft-don't-show"
+            className="flex space-x-2"
+          >
+            <Image
+              src="/images/question-gray5.svg"
+              alt=""
+              className="mr-2"
+              height={16}
+              width={16}
+            />
+            <B3 extraClasses="flex text-gray-syn5 hidden md:flex">
+              Why don’t NFTs show up here?
+            </B3>
+            <B4 extraClasses="flex text-gray-syn5 md:hidden">
+              Why don’t NFTs show up here?
+            </B4>
+          </div>
+
+          <ReactTooltip
+            id="why-nft-don't-show"
+            place="right"
+            effect="solid"
+            className="actionsTooltip max-w-88"
+            arrowColor="#222529"
+            backgroundColor="#222529"
+          >
+            <B3 extraClasses="text-white leading-5.75 font-normal">
+              Individual NFTs cannot be distributed to multiple members through
+              Syndicate today. If the club exchanges the NFTs for fungible
+              ERC-20 tokens, they will be available for distribution here.
+            </B3>
+          </ReactTooltip>
         </div>
       </div>
 
