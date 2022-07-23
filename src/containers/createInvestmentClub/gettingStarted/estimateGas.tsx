78cc0d8819204cda3dfc932c2499561e4927d48e
victorjambo <victorjambo@live.com>
2022-07-23 17:40:01 +0300

    update estimategas component

--- src/containers/createInvestmentClub/gettingStarted/estimateGas.tsx
+++ src/containers/createInvestmentClub/gettingStarted/estimateGas.tsx
@@ -5,17 +5,35 @@ import axios from 'axios';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-const EstimateGas = (props: { customClasses?: string }) => {
+export enum ContractMapper {
+  ClubERC20Factory,
+  DistributionsERC20,
+  DistributionsETH
+}
+
+interface Props {
+  contract?: ContractMapper; // TODO shouldn't be optional
+  customClasses?: string;
+  withFiatCurrency?: boolean;
+}
+
+const EstimateGas: React.FC<Props> = ({
+  contract = ContractMapper.ClubERC20Factory, // TODO shouldn't be optional
+  customClasses = '',
+  withFiatCurrency = false
+}) => {
   const {
     web3Reducer: {
       web3: { account, activeNetwork, web3 }
     },
     initializeContractsReducer: {
-      syndicateContracts: { clubERC20Factory }
+      syndicateContracts: {
+        clubERC20Factory,
+        distributionsERC20,
+        distributionsETH
+      }
     }
   } = useSelector((state: AppState) => state);
-
-  const { customClasses = '' } = props;
 
   const [gas, setGas] = useState(0); // 0.05 ETH (~$121.77)
   const [gasUnits, setGasUnits] = useState(0);
@@ -23,6 +41,31 @@ const EstimateGas = (props: { customClasses?: string }) => {
   const [nativeTokenPrice, setNativeTokenPrice] = useState<
     number | undefined
   >();
+  const [fiatAmount, setFiatAmount] = useState('');
+
+  const contracts = {
+    [ContractMapper.ClubERC20Factory]: {
+      syndicateContract: clubERC20Factory,
+      estimateGas: () => {
+        if (!clubERC20Factory) return;
+        clubERC20Factory.getEstimateGas(account, setGasUnits);
+      }
+    },
+    [ContractMapper.DistributionsERC20]: {
+      syndicateContract: distributionsERC20,
+      estimateGas: () => {
+        if (!distributionsERC20) return;
+        distributionsERC20.getEstimateGasDistributeERC20(account, setGasUnits);
+      }
+    },
+    [ContractMapper.DistributionsETH]: {
+      syndicateContract: distributionsETH,
+      estimateGas: () => {
+        if (!distributionsETH) return;
+        distributionsETH.getEstimateGasDistributeETH(account, setGasUnits);
+      }
+    }
+  };
 
   const processBaseFee = async (result) => {
     const baseFee = result.result;
@@ -31,29 +74,26 @@ const EstimateGas = (props: { customClasses?: string }) => {
   };
 
   const fetchGasUnitAndBaseFee = useCallback(async () => {
-    if (!clubERC20Factory) return;
-
     await Promise.all([
-      !account
-        ? setGasUnits(380000)
-        : clubERC20Factory.getEstimateGas(account, setGasUnits),
+      !account ? setGasUnits(380000) : contracts[contract].estimateGas(),
       axios
         .get(
           `${activeNetwork.blockExplorer.api}/api?module=proxy&action=eth_gasPrice`
         )
         .then((res) => processBaseFee(res.data))
         .catch(() => 0),
-      getNativeTokenPrice(activeNetwork.chainId)
-        .then((res) => setNativeTokenPrice(res))
-        .catch(() => 0)
+      withFiatCurrency &&
+        getNativeTokenPrice(activeNetwork.chainId)
+          .then((res) => setNativeTokenPrice(res))
+          .catch(() => 0)
     ]);
-  }, [account, clubERC20Factory]);
+  }, [account, contracts[contract].syndicateContract]);
 
   useEffect(() => {
     if (activeNetwork.chainId) {
       void fetchGasUnitAndBaseFee();
     }
-  }, [fetchGasUnitAndBaseFee, activeNetwork]);
+  }, [fetchGasUnitAndBaseFee, activeNetwork.chainId]);
 
   useEffect(() => {
     if (!gasUnits || !gasBaseFee) return;
@@ -66,6 +106,12 @@ const EstimateGas = (props: { customClasses?: string }) => {
     );
     setGas(+estimatedGas);
   }, [gasUnits, gasBaseFee]);
+
+  useEffect(() => {
+    if (withFiatCurrency && nativeTokenPrice) {
+      setFiatAmount((gas * nativeTokenPrice).toFixed(2));
+    }
+  }, [gas, nativeTokenPrice]);
 
   return (
     <button
@@ -81,10 +127,22 @@ const EstimateGas = (props: { customClasses?: string }) => {
           <span className="text-blue ">Estimated gas</span>
         </div>
 
-        <span className="mr-3 text-blue">
-          {gas
-            ? `${gas.toFixed(6)} ${activeNetwork.nativeCurrency.symbol}`
-            : `- ${activeNetwork.nativeCurrency.symbol}`}
+        <span className="mr-3 text-blue space-x-2">
+          <span>
+            {gas
+              ? `${gas.toFixed(6)} ${activeNetwork.nativeCurrency.symbol}`
+              : `- ${activeNetwork.nativeCurrency.symbol}`}
+          </span>
+          {withFiatCurrency && fiatAmount && (
+            <span>
+              (~
+              {Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD'
+              }).format(+fiatAmount)}
+              )
+            </span>
+          )}
         </span>
       </span>
     </button>
