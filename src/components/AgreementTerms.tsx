445d1d32ed9382f994c0f56f3fae963fdd7d6b0b
Oliver Qin <36147845+oliVRqin@users.noreply.github.com>
2022-08-31 08:47:52 -0700

    [ENG-4260][ENG-4256] updated legal agreements and added disclaimer modal in claim page (#1574)
    * updated legal agreements and added disclaimer modal in claim page
    
    * updated copy to new design
    
    * line fix
    
    * add padding

--- src/components/AgreementTerms.tsx
+++ src/components/AgreementTerms.tsx
@@ -1,9 +1,10 @@
 const AgreementTerms: React.FC<{
   hasAgreed: boolean;
   handleAgreed: () => void;
-}> = ({ hasAgreed, handleAgreed }) => {
+  customClassName?: string;
+}> = ({ hasAgreed, handleAgreed, customClassName }) => {
   return (
-    <div className="flex items-center space-between">
+    <div className="flex items-center space-between px-1">
       <input
         className="self-start bg-transparent rounded focus:ring-offset-0 cursor-pointer"
         onChange={handleAgreed}
@@ -16,40 +17,21 @@ const AgreementTerms: React.FC<{
         className="text-xs text-gray-syn4 leading-4.5 pl-4 cursor-pointer select-none text-left"
         onClick={handleAgreed}
       >
-        <div>
-          By accessing or using Syndicate’s app and its protocol, I agree that:
+        <div className={customClassName}>
+          By accessing or using Syndicate’s app and its protocol, I agree that I
+          will not use Syndicate’s app and its protocol to engage in illegal,
+          fraudulent or other wrongful conduct, including but not limited to (a)
+          distributing defamatory, obscene or unlawful content or content that
+          promotes bigotry, racism, misogyny or religious or ethnic hatred, (b)
+          transmitting any information or data that infringes any intellectual
+          property rights of any third party or that is otherwise libelous,
+          unlawful, or tortious, (c) stalking, harassment, or threatening others
+          with violence or abuse, or (d) violating any securities laws,
+          anti-money laundering laws, anti-terrorist financing laws,
+          anti-bribery or anti-boycott laws, or other applicable laws, and to
+          the extent necessary, I agree I will seek legal counsel to ensure
+          compliance with applicable laws in relevant jurisdictions.
         </div>
-
-        <ul className="list-disc list-outside ml-0 pl-6">
-          <li>
-            I will not violate securities laws by using Syndicate’s app and its
-            protocol, and to the extent necessary, will seek legal counsel to
-            ensure compliance with securities laws in relevant jurisdictions.
-          </li>
-          <li>
-            Any information or documentation provided has not been prepared with
-            my specific circumstances in mind and may not be suitable for use in
-            my personal circumstances.
-          </li>
-          <li>
-            Syndicate or any of its advisors (including Syndicate’s tax and
-            legal advisors) have not provided me any legal advice or other
-            professional advice, and no confidential or special relationship
-            between me and Syndicate or its affiliates or advisors exists.
-          </li>
-          <li>
-            I will not use Syndicate’s app and its protocol to engage in
-            illegal, fraudulent or other wrongful conduct, including but not
-            limited to (a) distributing defamatory, obscene or unlawful content
-            or content that promotes bigotry, racism, misogyny or religious or
-            ethnic hatred, (b) transmitting any information or data that
-            infringes any intellectual property rights of any third party or
-            that is otherwise libelous, unlawful, or tortious, (c) stalking,
-            harassment, or threatening others with violence or abuse, or (d)
-            violating any anti-money laundering laws, anti-terrorist financing
-            laws, anti-bribery or anti-boycott laws, or other applicable laws.
-          </li>
-        </ul>
       </button>
     </div>
   );
