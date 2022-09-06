bba6dfacfa15043eb6a621f7a90980950f727c57
Jillo Abdullahi <jillo.abdulla@gmail.com>
2022-09-06 20:25:41 +0300

    [ENG-4250] Update mobile view on success page (#1617)
    update mobile view on success page

--- src/components/collectives/create/success/index.tsx
+++ src/components/collectives/create/success/index.tsx
@@ -31,37 +31,49 @@ export const CollectivesCreateSuccess: React.FC<Props> = ({
         alt="Success"
         className="mx-auto mb-10 w-14 h-14"
       />
-      <div className="mb-6">
+      <div className="mb-6 px-6 md:px-0">
         <H3>Congratulations, {name}</H3>
         <B2 extraClasses="text-gray-syn3">
           A party of one is no fun - It’s time to invite some members
         </B2>
       </div>
-      <CopyLink
-        link={inviteLink}
-        updateCopyState={() => {
-          setShowCopiedState(!showCopiedState);
-          amplitudeLogger(INVITE_LINK_COPY, {
-            flow: Flow.COLLECTIVE_CREATE
-          });
-        }}
-        showCopiedState={showCopiedState}
-        copyButtonText="Copy invite link"
-      />
-      {loading ? (
-        <button className="primary-CTA-disabled mt-12 mb-6 w-full flex items-center justify-center content-center space-x-3">
-          <span className="">Preparing dashboard</span>
-          <Spinner
-            margin="my-0 items-center"
-            color="text-gray-syn4"
-            height="h-4"
-            width="w-4"
+      <div className="flex justify-center">
+        <div className="max-w-xs sm:max-w-full">
+          <CopyLink
+            link={inviteLink}
+            updateCopyState={() => {
+              setShowCopiedState(!showCopiedState);
+              amplitudeLogger(INVITE_LINK_COPY, {
+                flow: Flow.COLLECTIVE_CREATE
+              });
+            }}
+            showCopiedState={showCopiedState}
+            copyButtonText="Copy invite link"
           />
-        </button>
+        </div>
+      </div>
+
+      {loading ? (
+        <div className="w-full flex sm:block justify-center">
+          <button className="primary-CTA-disabled mt-12 mb-6 w-full flex items-center justify-center content-center space-x-3 ">
+            <span className="">Preparing dashboard</span>
+            <Spinner
+              margin="my-0 items-center"
+              color="text-gray-syn4"
+              height="h-4"
+              width="w-4"
+            />
+          </button>
+        </div>
       ) : (
-        <CtaButton extraClasses="mt-12 mb-6" onClick={CTAonClick}>
-          Manage on dashboard
-        </CtaButton>
+        <div className="w-full flex sm:block justify-center">
+          <CtaButton
+            extraClasses="mt-12 mb-6 px-6 md:px-0"
+            onClick={CTAonClick}
+          >
+            Manage on dashboard
+          </CtaButton>
+        </div>
       )}
       <a
         href={blockExplorerLink}
